b6f03b9043744b602c9017e192905cfe58106bd5
Stanley <owarieta24@outlook.com>
2026-01-02 01:49:05 +0100

    remove unwanted group hover color style rule

--- src/components/Skills/SkillsCard.jsx
+++ src/components/Skills/SkillsCard.jsx
@@ -3,7 +3,7 @@ const SkillsCard = ({ icon: Icon, skillName, index }) => {
         <div
             className="bg-dkchacol/20 hover:bg-goldmeat/35 duration-300 py-6 rounded-lg flex flex-col items-center gap-4 border border-white-shade/10 shadow-xl shadow-white-shade/3"
         >
-            <span className="w-14 h-14 lg:w-16 lg:h-16 flex justify-center items-center border border-purple rounded-lg bg-main-dkblack">
+            <span className="w-14 h-14 lg:w-16 lg:h-16 flex justify-center items-center border border-purple  rounded-lg bg-main-dkblack">
                 <Icon className="text-white-shade text-3xl lg:text-4xl" />
             </span>
 
